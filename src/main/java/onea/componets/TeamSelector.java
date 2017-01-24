6159e5f9d6958caa86107eb8c56a0534a854d507
Leon Felipe Cordero <zolopretroriano@gmail.com>
2017-01-23 17:46:56 -0700

    change to mvc

--- src/main/java/onea/componets/TeamSelector.java
+++
@@ -1,7 +0,0 @@
-package onea.componets;
-
-/**
- * Created by leon1a on 19/01/17.
- */
-public class TeamSelector {
-}
---
+++ src/main/java/onea/controller/HomeController.java
@@ -0,0 +1,18 @@
+package onea.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by leon on 23/01/2017.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public ModelAndView index(ModelAndView modelAndView){
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
